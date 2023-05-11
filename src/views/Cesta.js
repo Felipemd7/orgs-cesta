339eab0bb421c70e36e4d3872c6b7ba781d61024
Felipemd7 <felipemd114@gmail.com>
2023-05-11 10:58:07 -0300

    Limpar cache do Git

--- src/views/Cesta.js
+++ src/views/Cesta.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { Text, Image, StyleSheet, Dimensions, View } from 'react-native';
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width; //pegando dimensões da tela
 
 export default function Cesta(){
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+        <Text style={estilos.titulo}>Detalhe da Cesta</Text>       
+
+        <View style={estilos.espacoCesta}>            
+        <Text style={estilos.nomeCesta}>Cesta de Verduras</Text>
+
+        <View style={estilos.orgFazenda}>
+         <Image style={estilos.logoFazenda} source={logo}></Image>   
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
+
+        <Text style={estilos.descricao}>
+        Uma cesta com produtos 
+        selecionados cuidadosamente da 
+        fazenda direto para a cozinha.
+        </Text>
+        
+        <Text style={estilos.preco}>$40,00</Text>
+        </View>
+        
     </>
+
     
 }
 
@@ -25,7 +45,45 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         color: "white",
         fontWeight: "bold",
-        padding: 50
+        padding: 50,    
         
+    },
+    nomeCesta: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontFamily: "MontserratBold"
+    },
+    espacoCesta: {
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+    },
+    orgFazenda:{
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular",
+         
+    },
+    logoFazenda:{
+        width: 32,
+        height: 32,
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
     }
-});
+
+}); 
